d179e470d687537b6cec9db419d38a508f7e4e76
Will <will@containerlabs.io>
2022-05-23 11:23:12 -0700

    changing env to envs

--- pkg/application/convert_test.go
+++ pkg/application/convert_test.go
@@ -27,13 +27,13 @@ func TestConvertToBundle(t *testing.T) {
 						Type:     "foo",
 						Field:    "some-field",
 						Required: false,
-						Env:      []application.ApplicationDependenciesEnvs{},
+						Envs:      []application.ApplicationDependenciesEnvs{},
 					},
 					{
 						Type:     "bar",
 						Field:    "another-field",
 						Required: true,
-						Env:      []application.ApplicationDependenciesEnvs{},
+						Envs:      []application.ApplicationDependenciesEnvs{},
 					},
 				},
 				Params: map[string]interface{}{
