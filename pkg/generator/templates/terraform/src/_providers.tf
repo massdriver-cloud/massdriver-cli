07c74aecd7af7d0c3550910fa424891e7d72f5bc
Cory O'Daniel <cory@coryodaniel.com>
2022-05-25 12:22:53 -0700

    adding region from correct source in providers

--- pkg/generator/templates/terraform/src/_providers.tf
+++ pkg/generator/templates/terraform/src/_providers.tf
@@ -33,7 +33,7 @@ terraform {
 }
 
 # provider "aws" {
-#   region     = var.aws_region
+#   region     = var.network.specs.aws.region
 #   assume_role {
 #     role_arn    = var.aws_authentication.data.arn
 #     external_id = var.aws_authentication.data.external_id
