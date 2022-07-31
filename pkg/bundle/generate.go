da6a78508b38722c72bec9cf224f9cf0e9dc20ad
Will <will@containerlabs.io>
2022-07-30 17:48:18 -0700

    remove dead code

--- pkg/bundle/generate.go
+++ pkg/bundle/generate.go
@@ -4,22 +4,12 @@ import (
 	"embed"
 	"io/fs"
 
-	// "text/template"
-
 	"github.com/massdriver-cloud/massdriver-cli/pkg/template"
 )
 
 // note to all: option in go 1.18 will load hidden files so we dont have to include `cp` instructions in readme for pre-commit.
 //go:embed templates/* templates/terraform/.pre-commit-config.yaml templates/terraform/.gitignore templates/terraform/.github/workflows/build.yaml templates/terraform/.github/workflows/publish.yaml templates/terraform/src/_artifacts.tf templates/terraform/src/_providers.tf
 var templatesFs embed.FS
-
-// type TemplateData struct {
-// 	Name        string
-// 	Description string
-// 	Access      string
-// 	Type        string
-// 	OutputDir   string
-// }
 
 func Generate(data *template.Data) error {
 	templateDir, _ := fs.Sub(fs.FS(templatesFs), "templates/terraform")
