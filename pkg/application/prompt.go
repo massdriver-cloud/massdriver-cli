6a54ab8a7e36488a9527d8072a3dffc7598c1f9a
Jake Ferriero <jtferriero@gmail.com>
2022-09-08 14:30:56 -0700

    Update pkg/application/prompt.go

--- pkg/application/prompt.go
+++ pkg/application/prompt.go
@@ -145,6 +145,7 @@ func getOutputDir(t *template.Data) error {
 	return nil
 }
 
+// note, does not preserve ordering
 func remove(s []string, i int) []string {
 	s[i] = s[len(s)-1]
 	return s[:len(s)-1]
