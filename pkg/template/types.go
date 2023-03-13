e8fa5d9bdedace06ab7d4065b3bc51133492efc1
Andreas <andreas@massdriver.cloud>
2023-03-13 18:17:12 +0000

    Strongly typed connections
    instead of using a map, we strongly type connections as
    
    ```
    Connection {
        Name
        ArtifactDefinition
    }
    ```

--- pkg/template/types.go
+++ pkg/template/types.go
@@ -1,16 +1,21 @@
 package template
 
 type Data struct {
-	Name            string                 `json:"name"`
-	Description     string                 `json:"description"`
-	Access          string                 `json:"access"`
-	Location        string                 `json:"location"`
-	TemplateName    string                 `json:"templateName"`
-	TemplateSource  string                 `json:"templateSource"`
-	OutputDir       string                 `json:"outputDir"`
-	Type            string                 `json:"type"`
-	Connections     map[string]interface{} `json:"connections"`
-	CloudPrefix     string                 `json:"cloudPrefix"`
-	RepoName        string                 `json:"repoName"`
-	RepoNameEncoded string                 `json:"repoNameEncoded"`
+	Name            string       `json:"name"`
+	Description     string       `json:"description"`
+	Access          string       `json:"access"`
+	Location        string       `json:"location"`
+	TemplateName    string       `json:"templateName"`
+	TemplateSource  string       `json:"templateSource"`
+	OutputDir       string       `json:"outputDir"`
+	Type            string       `json:"type"`
+	Connections     []Connection `json:"connections"`
+	CloudPrefix     string       `json:"cloudPrefix"`
+	RepoName        string       `json:"repoName"`
+	RepoNameEncoded string       `json:"repoNameEncoded"`
+}
+
+type Connection struct {
+	Name               string `json:"name"`
+	ArtifactDefinition string `json:"artifact_definition"`
 }
